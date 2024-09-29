3c339fe6b589082c4d2be0fdc58fddae1a830f04
Jan Vrba <jvhona@gmail.com>
2024-09-29 16:51:16 +0200

    pridan hlavni program + opravy chyb

--- nakupni_seznam.py
+++ nakupni_seznam.py
@@ -15,18 +15,45 @@ def odebrat_polozku(seznam):
 
 def zobrazit_seznam(seznam):
     if seznam:
-        print(seznam)
+        print(f"Tohle je tvůj nákupní seznam {seznam}.")
     else:
         print("Seznam je prázdný.")
 
 
 def seradit_seznam(seznam):
     seznam.sort()
+    print("Nákupní seznam byl abecedně seřazen.")
 
 def zobrazit_pocet_polozek(seznam):
     print(f"V seznamu je {len(seznam)} položek.")
 
 def ukoncit_program():
-    exit
+    print("Program byl ukončen.")
+    exit()
     
 #hlavní program
+seznam = []
+
+while True:
+    print("\n1 pro přidání položky.")
+    print("2 pro odebrání položky.")
+    print("3 pro zobrazení nákupního seznamu.")
+    print("4 pro abecední seřazení nákupního seznamu.")
+    print("5 pro zobrazení počtu položek v nákupním seznamu.")
+    print("6 pro ukončení programu.")
+
+    vyber = input("\nVyber možnost 1 až 6: ")
+    if vyber == "1":
+        pridat_polozku(seznam)
+    elif vyber == "2":
+        odebrat_polozku(seznam)
+    elif vyber == "3":
+        zobrazit_seznam(seznam)
+    elif vyber == "4":
+        seradit_seznam(seznam)
+    elif vyber == "5":
+        zobrazit_pocet_polozek(seznam)
+    elif vyber == "6":
+        ukoncit_program()
+    else:
+        print("Neplatný výběr možnosti. Zkus to znovu.")
